bf353bb356e8ac9e21d78da3f8ca84a6d3d68813
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-02 01:42:41 +0600

    chefs json data added

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express()
 const port = 5000
 const cors = require('cors')
 
+const chefs = require('./data/chefs.json')
+
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('yumpies is running')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 })
 
 app.listen(port, () => {
