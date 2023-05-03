961375a91f82b81ca4bc8a164ccddb3dac3564d0
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-03 16:19:59 +0600

    json updated

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const port = 5000
 const cors = require('cors')
 
 const chefs = require('./data/chefs.json')
-const recipes = require('./data/recipe.json')
 
 app.use(cors());
 
@@ -16,22 +15,11 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
-app.get('/recipes', (req, res) => {
-    res.send(recipes);
-})
-
 app.get('/chefs/:id', (req, res) => {
     const id = req.params.id;
     const chefInfo = chefs.find(chef => chef.id == id);
     res.send(chefInfo);
 })
-
-app.get('/recipes/:id', (req, res) => {
-    const id = req.params.id;
-    const chefsRecipe = recipes.filter(r => r.chef_id == id);
-    res.send(chefsRecipe);
-})
-
 
 app.listen(port, () => {
     console.log(`Listening to the port ${port}`);
